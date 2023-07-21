29b19ed20089b0db756a7d9fe1f48b40ced8d730
SanketLakhani <sanketlakhani80@gmail.com>
2023-07-21 15:37:01 +0530

    add one back to top floating button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,10 +39,11 @@ const toggleNavbar = function () {
 addEventOnElements(navTogglers, "click", toggleNavbar);
 
 /**
- * HEADER
+ * HEADER & BACK TOP BTN
  */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 let lastScrollPos = 0;
 
@@ -61,9 +62,11 @@ const hideHeader = function () {
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 50) {
     header.classList.add("active");
+    backTopBtn.classList.add("active");
     hideHeader();
   } else {
     header.classList.remove("active");
+    backTopBtn.classList.remove("active");
   }
 });
 
